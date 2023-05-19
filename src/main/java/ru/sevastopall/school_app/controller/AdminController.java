a036933d29c956dd507860017cb60d15060695ad
SevaStopAll <sevastopallrh@yandex.ru>
2023-05-19 11:59:15 +0400

    add controllers and templates

---
+++ src/main/java/ru/sevastopall/school_app/controller/AdminController.java
@@ -0,0 +1,11 @@
+package ru.sevastopall.school_app.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+@AllArgsConstructor
+public class AdminController {
+}
