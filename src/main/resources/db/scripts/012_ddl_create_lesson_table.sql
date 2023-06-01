d83b3594870f7f379659cb96a9baf3fc10cd8163
SevaStopAll <sevastopallrh@yandex.ru>
2023-06-01 11:57:54 +0400

    Start Reworking Lesson class.

--- src/main/resources/db/scripts/012_ddl_create_lesson_table.sql
+++ src/main/resources/db/scripts/012_ddl_create_lesson_table.sql
@@ -1,5 +1,7 @@
 create table if not exists lesson(
     id serial primary key,
+    lessonDate date,
+    number int,
     name varchar(256) UNIQUE,
     subject_id int references subject(id),
     teacher_id int references teacher(id),
