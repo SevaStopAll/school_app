5ce3acb69dc76bec3443cca0866a9c8b906da8ce
SevaStopAll <sevastopallrh@yandex.ru>
2023-06-22 11:48:32 +0400

    Started creating information for downloading.

--- src/main/java/ru/sevastopall/school_app/controller/TeacherController.java
+++ src/main/java/ru/sevastopall/school_app/controller/TeacherController.java
@@ -1,6 +1,10 @@
 package ru.sevastopall.school_app.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +13,12 @@ import ru.sevastopall.school_app.domain.Mark;
 import ru.sevastopall.school_app.domain.Student;
 import ru.sevastopall.school_app.domain.Teacher;
 import ru.sevastopall.school_app.service.*;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Controller
 @RequestMapping("/teacher")
@@ -70,5 +80,51 @@ public class TeacherController {
         model.addAttribute("marks", marks.findByTeacher(teacher));
         model.addAttribute("homeworks", homeworks.findByTeacher(teacher));
         return "teacher/one";
+    }
+
+    @GetMapping("/file/{id}")
+    public ResponseEntity<byte[]> getInfo(@PathVariable int id) throws IOException {
+        var teacherOptional = teachers.findByUser(users.findById(id).get());
+        Teacher teacher = teacherOptional.get();
+        try (FileWriter out = new FileWriter("./src/main/resources/data/report.txt")) {
+            out.append("Оценки");
+            out.append(System.lineSeparator());
+            out.append("Оценка");
+            out.append(" ");
+            out.append("Предмет");
+            out.append(" ");
+            out.append("Ученик");
+            out.append(System.lineSeparator());
+            for (Mark mark : marks.findByTeacher(teacher)) {
+                out.append(String.valueOf(mark.getScore().getId()));
+                out.append(mark.getSubject().getName());
+                out.append(mark.getStudent().getLastName());
+                out.append(System.lineSeparator());
+            }
+            out.append(System.lineSeparator());
+            out.append(System.lineSeparator());
+            out.append("Домашнее задание");
+            out.append(System.lineSeparator());
+            out.append("Предмет");
+            out.append(" ");
+            out.append("Описание");
+            out.append(" ");
+            for (Homework homework : homeworks.findByTeacher(teacher)) {
+                out.append(homework.getSubject().getName());
+                out.append(homework.getDescription());
+                out.append(System.lineSeparator());
+            }
+
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        var content = Files.readAllBytes(Path.of("./src/main/resources/data/report.txt"));
+        HttpHeaders headers = new HttpHeaders();
+        String filename = "Report.txt";
+        headers.setContentDispositionFormData(filename, filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        headers.setContentType(MediaType.parseMediaType("application/txt"));
+        return new ResponseEntity<>(content,headers, HttpStatus.OK);
     }
 }
