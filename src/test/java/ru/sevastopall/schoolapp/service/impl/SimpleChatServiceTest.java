cec65a30137146a8c349dbb8214087bb15a0dbd2
sevastopallrh@yandex.ru <sevastopallrh@yandex.ru>
2023-12-13 20:36:36 +0300

    Check and fix some tests. Leave one task for the future.

--- src/test/java/ru/sevastopall/schoolapp/service/impl/SimpleChatServiceTest.java
+++ src/test/java/ru/sevastopall/schoolapp/service/impl/SimpleChatServiceTest.java
@@ -1,8 +1,10 @@
 package ru.sevastopall.schoolapp.service.impl;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.sevastopall.schoolapp.domain.Chat;
+import ru.sevastopall.schoolapp.domain.ChatMessage;
 import ru.sevastopall.schoolapp.domain.Message;
 import ru.sevastopall.schoolapp.domain.User;
 import ru.sevastopall.schoolapp.integration.IntegrationTestBase;
@@ -14,42 +16,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SimpleChatServiceTest extends IntegrationTestBase {
 
     @Autowired private SimpleChatService simpleChatService;
-    @Autowired private SimpleMessageService simpleMessageService;
+    @Autowired private SimpleChatMessageService simpleChatMessageService;
+    @Autowired private SimpleUserService simpleUserService;
 
     @Test
     public void whenSave() {
     //Arrange
+    Chat chat = new Chat();
 
     //Act
-    simpleChatService.save();
-    //Assert
+    Chat result = simpleChatService.save(chat);
 
+    //Assert
+    Assertions.assertThat(result).isEqualTo(chat);
     }
 
     @Test
     public void whenFindById() {
         //Arrange
+        Chat chat = new Chat();
+        Chat savedChat = simpleChatService.save(chat);
+        long aimId = savedChat.getId();
 
         //Act
-        simpleChatService.findById();
+        Chat result = simpleChatService.findById(aimId);
+
         //Assert
+        Assertions.assertThat(result).isEqualTo(savedChat);
+
     }
+
     @Test
     public void whenFindByParticipantsContaining() {
         //Arrange
+        Chat chat = new Chat();
+        User user1 = new User();
+        User participant = simpleUserService.create(user1).get();
+        chat.setParticipants(List.of(participant));
+        Chat savedChat = simpleChatService.save(chat);
 
         //Act
-        simpleChatService.findByParticipantsContaining();
+        List<Chat> result = simpleChatService.findByParticipantsContaining(participant);
+
         //Assert
+        Assertions.assertThat(result.contains(savedChat)).isTrue();
     }
 
     @Test
     public void whenFindByMessagesContaining() {
         //Arrange
+        Chat chat = new Chat();
+        ChatMessage chatMessage = new ChatMessage();
+        ChatMessage savedChatMessage = simpleChatMessageService.save(chatMessage);
+        chat.setMessages(List.of(savedChatMessage));
+        Chat savedChat = simpleChatService.save(chat);
 
         //Act
-        simpleChatService.findByMessagesContaining();
+        List<Chat> result = simpleChatService.findByMessagesContaining(savedChatMessage);
+
         //Assert
+        Assertions.assertThat(result.contains(savedChat)).isTrue();
     }
 
 
